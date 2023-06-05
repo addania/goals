b4d5c47618645e7d39cc92632dcd004853eb0e1b
addania <mirka.schw@gmail.com>
2023-06-05 18:39:24 +0200

    Fix TS error for Confetti using Engine type

--- src/pages/components/animationEffects/Confetti.tsx
+++ src/pages/components/animationEffects/Confetti.tsx
@@ -1,8 +1,9 @@
 import Particles from "react-tsparticles";
+import type { Engine } from "tsparticles-engine";
 import { loadConfettiPreset } from "tsparticles-preset-confetti";
 
 export const Confetti = () => {
-  const customInit = async (engine) => {
+  const customInit = async (engine: Engine) => {
     // this adds the preset to tsParticles, you can safely use the
     await loadConfettiPreset(engine);
   };
